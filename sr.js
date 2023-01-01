e89e918dcc9af0e7de6155e5d53ec1ff39279322
Gabriel Moura <100838337+musgodev@users.noreply.github.com>
2023-01-01 15:06:02 -0300

    Delete sr.js

--- sr.js
+++
@@ -1,9 +0,0 @@
-const sr = ScrollReveal({
-    origin: 'left',
-    distance: '50px',
-    duration: 2000,
-    reset: true
-});
-
-ScrollReveal().reveal('.main__content', { delay: 100 });
-ScrollReveal().reveal('.card', { delay: 100 });
